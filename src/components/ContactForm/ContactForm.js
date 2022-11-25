362c28039250baf71dc9b67fc78752c58bfc7b53
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2022-11-25 17:33:04 +0100

    change proptypes

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,60 +1,65 @@
+import { useState } from 'react';
+import {
+  FormAddContact,
+  Label,
+  Input,
+  ButtonSubmit,
+} from './ContactForm.styled';
 
-import {useState} from "react";
-import {FormAddContact, Label, Input, ButtonSubmit } from "./ContactForm.styled";
-
-export function ContactForm ({onSubmitDate}){
+export function ContactForm({ onSubmitDate }) {
   const [name, setName] = useState('');
-  const [number, setNumber] = useState('')
+  const [number, setNumber] = useState('');
 
-     const  hendleNameChange = e =>{
-        const {name, value} = e.target;
-        // this.setState({[name]: value})
-        if(name === 'name'){
-          setName(value)
-          return
-        }
-        if(name === 'number'){
-          setNumber(value)
-        }
-      }
-    
-     const  Clear = () =>{
-       setName('');
-       setNumber('');
-      }
+  const hendleNameChange = e => {
+    const { name, value } = e.target;
+    if (name === 'name') {
+      setName(value);
+      return;
+    }
+    if (name === 'number') {
+      setNumber(value);
+    }
+  };
 
-    const  hendlOnSubmit = e =>{
-        e.preventDefault();
-onSubmitDate({name, number});
-      Clear();
-      }
+  const Clear = () => {
+    setName('');
+    setNumber('');
+  };
 
-    return (
-<FormAddContact onSubmit={hendlOnSubmit} >
-<Label htmlFor="idLabelName">Name</Label>
-    <Input
-    id="idLabelName"
-      type="text"
-      name="name"
-      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-      required
-      value={name}
-      onChange={hendleNameChange}
-    />
-<Label htmlFor="idLabelNumber">Number</Label>
-    <Input
-    id="idLabelNumber"
-      type="tel"
-      name="number"
-      pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-      title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-      required
-      value={number}
-      onChange={hendleNameChange}
-    />
-   
-    <ButtonSubmit type="submit"> Add contact </ButtonSubmit>
-   </FormAddContact>
-    )
+  const hendlOnSubmit = e => {
+    e.preventDefault();
+    onSubmitDate({ name, number });
+    Clear();
+  };
+
+  return (
+    <FormAddContact onSubmit={hendlOnSubmit}>
+      <Label htmlFor="idLabelName">Name</Label>
+      <Input
+        id="idLabelName"
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        value={name}
+        onChange={hendleNameChange}
+      />
+      <Label htmlFor="idLabelNumber">Number</Label>
+      <Input
+        id="idLabelNumber"
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        value={number}
+        onChange={hendleNameChange}
+      />
+
+      <ButtonSubmit type="submit"> Add contact </ButtonSubmit>
+    </FormAddContact>
+  );
 }
+
+ContactForm.propTypes = { onSubmit: PropTypes.func };
--- src/components/ContactList/ContactList .js
+++ src/components/ContactList/ContactList .js
@@ -1,18 +1,31 @@
-import { ButtonDelete, List, ListItem } from "./ContactList.styled";
+import { ButtonDelete, List, ListItem } from './ContactList.styled';
 import PropTypes from 'prop-types';
 
-export const ContactList = ({Array, onDelete}) =>(
-        <List>
-            {Array.length > 0 ? (
-(Array).map(({id, name, number}) => (<ListItem key={id}><span> {name}: {number}</span> <ButtonDelete onClick={()=> onDelete(id)}> ❌</ButtonDelete> </ListItem>))
-            ):(
-<p>You don't have any contact</p>
-            )
-            }
-    </List>
-    )
+export const ContactList = ({ Array, onDelete }) => (
+  <List>
+    {Array.length > 0 ? (
+      Array.map(({ id, name, number }) => (
+        <ListItem key={id}>
+          <span>
+            {' '}
+            {name}: {number}
+          </span>{' '}
+          <ButtonDelete onClick={() => onDelete(id)}> ❌</ButtonDelete>{' '}
+        </ListItem>
+      ))
+    ) : (
+      <p>You don't have any contact</p>
+    )}
+  </List>
+);
 
-    ContactList.propTypes = {
-        Array: PropTypes.arrayOf(PropTypes.object).isRequired,
-        onDelete: PropTypes.func.isRequired,
-    }
+ContactList.propTypes = {
+  Array: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      id: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDelete: PropTypes.func.isRequired,
+};
